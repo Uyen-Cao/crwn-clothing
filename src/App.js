c4c9918368dcb2c8efc8af69fa70aa4174ea5a0c
Macbook Pro <macbookpro@MacBook-Pro-cua-Macbook.local>
2021-10-12 02:03:44 +0700

    created sign-in-and-sign-up page, customed form-input and button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import SignInSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -17,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   );
