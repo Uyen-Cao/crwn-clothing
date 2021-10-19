4ff2d4fe5aca4fed163281a06a516533b5aa8024
Macbook Pro <macbookpro@MacBook-Pro-cua-Macbook.local>
2021-10-20 00:53:31 +0700

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding component with new redux flow for directory/shop

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import CollectionPreview from "../preview-collection/preview-collection.component";
+
+import "./collection-overview.styles.scss";
+
+const CollectionOverview = ({ collections }) => {
+  console.log(collections);
+  return (
+    <div className="collection-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => {
+        return <CollectionPreview key={id} {...otherCollectionProps} />;
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
